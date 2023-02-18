c795e7179cd8a579447046ffed5f7a237ce966ac
princesamiurrehman <111885793+princesamiurrehman@users.noreply.github.com>
2023-02-18 21:13:56 +0500

    Update settings.py

--- easysnp/settings.py
+++ easysnp/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 import os
 
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static', 'static')
 
 
 # Default primary key field type
